33a75b0f084bc0681327db7f374ad4e263f77361
Mehedi Hasan <thecodermehedi@gmail.com>
2025-07-05 01:36:22 +0600

    Add service records routes to the main router

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,6 @@
+import { BikesRoutes } from "@app/modules/bikes/bikes.routes";
 import { CustomersRoutes } from "@app/modules/customers/customers.routes";
+import { ServiceRecordsRoutes } from "@app/modules/service_records/service_records.routes";
 import createRouter, { type TRouter } from "@shared/createRouter";
 
 const router = createRouter();
@@ -7,6 +9,8 @@ type TRoute = { path: string; router: TRouter };
 
 const moduleRoutes: TRoute[] = [
 	{ path: "/customers", router: CustomersRoutes },
+	{ path: "/bikes", router: BikesRoutes },
+	{ path: "/services", router: ServiceRecordsRoutes },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.router));
