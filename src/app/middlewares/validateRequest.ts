d7721411fe79fc117188b4b07d1a57c17a07e5ac
Mehedi Hasan <thecodermehedi@gmail.com>
2025-07-04 21:25:59 +0600

    Enhance validateRequest middleware to include query and params validation in schema parsing

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -4,10 +4,14 @@ import type { AnyZodObject } from "zod";
 const validateRequest = (schema: AnyZodObject): RequestHandler => {
 	return async (req, _res, next) => {
 		try {
-			await schema.parseAsync({ body: req.body });
-			return next();
+			await schema.parseAsync({
+				body: req.body,
+				query: req.query,
+				params: req.params,
+			});
+			next();
 		} catch (error) {
-			return next(error);
+			next(error);
 		}
 	};
 };
