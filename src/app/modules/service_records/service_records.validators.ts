bd13779cded8862b480fa3932e018023791884fe
Mehedi Hasan <thecodermehedi@gmail.com>
2025-07-05 01:36:14 +0600

    Add validation schemas for service records creation and updates

---
+++ src/app/modules/service_records/service_records.validators.ts
@@ -0,0 +1,34 @@
+import { ServiceStatus } from "@generated/prisma";
+import z from "zod";
+
+const createServiceRecordValidationSchema = z.object({
+	body: z.object({
+		bikeId: z.string().uuid(),
+		serviceDate: z.string().datetime(),
+		description: z.string().min(1).max(500),
+		status: z.nativeEnum(ServiceStatus),
+	}),
+});
+
+const serviceRecordIdParamsValidationSchema = z.object({
+	params: z.object({
+		id: z.string().uuid(),
+	}),
+});
+
+const updateServiceRecordByIdValidationSchema = z.object({
+	params: z.object({
+		id: z.string().uuid(),
+	}),
+	body: z
+		.object({
+			completionDate: z.string().datetime().optional(),
+		})
+		.optional(),
+});
+
+export const ServiceRecordsValidators = {
+	createServiceRecordValidationSchema,
+	serviceRecordIdParamsValidationSchema,
+	updateServiceRecordByIdValidationSchema,
+};
