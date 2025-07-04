f421e23def815861d12fb358f28b0c380c6e8cab
Mehedi Hasan <thecodermehedi@gmail.com>
2025-07-05 01:36:10 +0600

    Implement service records routes with validation and controllers

---
+++ src/app/modules/service_records/service_records.routes.ts
@@ -0,0 +1,29 @@
+import validateRequest from "@app/middlewares/validateRequest";
+import createRouter from "@shared/createRouter";
+import { ServiceRecordsControllers } from "./service_records.controllers";
+import { ServiceRecordsValidators } from "./service_records.validators";
+
+const router = createRouter();
+
+router.post(
+	"/",
+	validateRequest(ServiceRecordsValidators.createServiceRecordValidationSchema),
+	ServiceRecordsControllers.createServiceRecord,
+);
+router.get("/", ServiceRecordsControllers.getServiceRecords);
+router.get(
+	"/:id",
+	validateRequest(
+		ServiceRecordsValidators.serviceRecordIdParamsValidationSchema,
+	),
+	ServiceRecordsControllers.getServiceRecordById,
+);
+router.put(
+	"/:id/complete",
+	validateRequest(
+		ServiceRecordsValidators.updateServiceRecordByIdValidationSchema,
+	),
+	ServiceRecordsControllers.updateServiceRecordById,
+);
+
+export const ServiceRecordsRoutes = router;
